e06e3da069521b8f5592458427f46fd1fef76088
Jcook894 <Jcook894@icloud.com>
2016-11-30 16:14:00 -0500

    Added sprite to the canvas.

--- js/game.js
+++ js/game.js
@@ -1,12 +1,12 @@
-var game = new Phaser.Game(650, 350, Phaser.AUTO);
+var game = new Phaser.Game(650, 350, Phaser.CANVAS);
 
 var gameState = {
   preload: function(){
-
+    game.load.spritesheet('Little Jay', 'assets/little_jay.png', 32, 50);
 
   },
   create: function(){
-
+    game.add.sprite(0,0,'Little Jay');
   },
 
   update: function(){
