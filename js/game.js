919810e0700af1738628e841e7695ac95253a6c4
Jcook894 <Jcook894@icloud.com>
2016-11-30 19:06:03 -0500

    Gave the sprite a gun.

--- js/game.js
+++ js/game.js
@@ -2,11 +2,11 @@ var game = new Phaser.Game(650, 350, Phaser.CANVAS);
 
 var gameState = {
   preload: function(){
-    game.load.spritesheet('Little Jay', 'assets/little_jay.png', 32, 50);
+    this.load.spritesheet('Little Jay', 'assets/little_jay.png', 32, 50);
 
   },
   create: function(){
-    game.add.sprite(0,0,'Little Jay');
+    game.add.sprite(250, 250 ,'Little Jay');
   },
 
   update: function(){
