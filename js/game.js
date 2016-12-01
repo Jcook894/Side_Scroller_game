ce3f45a76679e2c753ecf999e451f0b502093eda
Jcook894 <Jcook894@icloud.com>
2016-12-01 13:52:35 -0500

    Added hearts to the game.

--- js/game.js
+++ js/game.js
@@ -1,11 +1,13 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS);
 var platforms;
 var player;
+var hearts;
 
 
 var gameState = {
 
   preload: function(){
+    game.load.image('heart', 'assets/heart.png');
     game.load.image('grass', 'assets/platform.png');
     game.load.image('sky', 'assets/sky.png');
     game.load.spritesheet('Little Jay', 'assets/Mac.png', 100, 100);
@@ -38,6 +40,16 @@ var gameState = {
     player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
 
+    hearts = game.add.group();
+
+    hearts.enableBody = true;
+
+    for(var i = 0; i < 12; i++){
+      var heart = hearts.create(i * 70, 0, 'heart');
+
+      heart.body.gravity.y = 6;
+
+    }
 
   },
 
@@ -59,7 +71,7 @@ var gameState = {
     }
 
     if(cursors.up.isDown && player.body.touching.down && onPlatform ){
-      player.body.velocity.y = 500;
+      player.body.velocity.y = -300;
     }
 
 
