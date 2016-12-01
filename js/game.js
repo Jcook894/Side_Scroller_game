901152484515aebdd58343895443141adbf46f15
Jcook894 <Jcook894@icloud.com>
2016-12-01 12:37:03 -0500

    Added player collision with the ground.

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS);
 var platforms;
+var player;
 
 
 var gameState = {
@@ -7,18 +8,18 @@ var gameState = {
   preload: function(){
     game.load.image('grass', 'assets/platform.png');
     game.load.image('sky', 'assets/sky.png');
-    game.load.spritesheet('Little Jay', 'assets/little_jay.png', 32, 50);
+    game.load.spritesheet('Little Jay', 'assets/Mac.png', 100, 100);
 
   },
   create: function(){
+
+
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.add.image(0, 0, 'sky');
 
     platforms = game.add.group();
     platforms.enableBody = true;
-
-
 
     var grass = platforms.create(0, game.world.height - 64, 'grass');
     grass.scale.setTo(2,2);
@@ -29,13 +30,24 @@ var gameState = {
     ledge = platforms.create(-150, 250, 'grass');
     ledge.body.immovable = true;
 
+    player = game.add.sprite(32, game.world.height -175, "Little Jay");
+
+    game.physics.arcade.enable(player);
+
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
 
 
-
-    this.add.sprite(250, 250 ,'Little Jay');
   },
 
   update: function(){
+
+    var onPlatform = game.physics.arcade.collide(player,platforms);
+
+    player.body.velocity = 0;
+
+
 
 
   }
