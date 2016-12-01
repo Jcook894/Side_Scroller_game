41cefc86c3b9721544f03f0af779263ec4e18914
Jcook894 <Jcook894@icloud.com>
2016-12-01 12:49:27 -0500

    Added movement to Mac.

--- js/game.js
+++ js/game.js
@@ -42,10 +42,26 @@ var gameState = {
   },
 
   update: function(){
+    cursors = game.input.keyboard.createCursorKeys();
 
     var onPlatform = game.physics.arcade.collide(player,platforms);
 
-    player.body.velocity = 0;
+    player.body.velocity.x = 0;
+
+    if(cursors.left.isDown){
+      player.body.velocity.x = -150;
+
+
+    }
+
+    else if(cursors.right.isDown){
+      player.body.velocity.x = 150;
+    }
+
+    if(cursors.up.isDown && player.body.touching.down && onPlatform ){
+      player.body.velocity.y = 500;
+    }
+
 
 
 
